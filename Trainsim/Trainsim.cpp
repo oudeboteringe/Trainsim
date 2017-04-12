1a5fb81cd8822b63cfa04bec614dbadcef9d259d
oudeboteringe <michiel_72@hotmail.com>
2017-04-12 16:59:46 +0100

    Initial commit

---
+++ Trainsim/Trainsim.cpp
@@ -0,0 +1,15 @@
+// Trainsim.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Press any key to exit." << endl;
+  _getch();
+  return 0;
+}
+
